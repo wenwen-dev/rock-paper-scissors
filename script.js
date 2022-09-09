d7a2edd2c5828706bee2d20b824e8b25ffbf3a43
Wenwen Wang <wenwen.wangwen@gmail.com>
2022-09-09 09:40:22 -0700

    Complete game's codes

---
+++ script.js
@@ -0,0 +1,75 @@
+function getComputerChoice() {
+  let randomNumber = Math.random();
+  let computerChoice;
+  randomNumber < 0.34 ? computerChoice = 'rock' : 
+  randomNumber < 0.68 ? computerChoice = 'paper' :
+  computerChoice = 'scissors';
+  return computerChoice;
+}
+
+function playOneRound(playerChoice, computerSelection){
+  let playerSelection = playerChoice.toLowerCase();
+  switch(true) {
+    case playerSelection === computerSelection:
+      return `It\'s a tie! Player: ${playerSelection}, Computer: ${computerSelection}. `;
+      break;
+    case playerSelection === 'rock' && computerSelection === 'scissors':
+    case playerSelection === 'paper' && computerSelection === 'rock':
+    case playerSelection === 'scissors' && computerSelection === 'paper':
+      return `You win! Player: ${playerSelection}, Computer: ${computerSelection}.  ${playerSelection} beats ${computerSelection}`;
+      break;
+    default:
+      return `You lose! Player: ${playerSelection}, Computer: ${computerSelection}.  ${computerSelection} beats ${playerSelection}`;
+  }
+}
+
+
+function game() {
+  let playerInput;
+  let playerCounter = 0;
+  let computerCounter = 0;
+  for (let i = 0; i < 5; i++) {
+    playerInput = prompt('Type rock, paper or scissors:');
+    let result = playOneRound(playerInput, getComputerChoice());
+    console.log(result);
+    if (result.indexOf('tie') !== -1) {
+      playerCounter++;
+      computerCounter++;
+    }
+    else if (result.indexOf('win') !== -1) {
+      playerCounter++;
+    }
+    else
+      computerCounter++;
+  }
+  if (playerCounter === 5 && computerCounter < 5)
+    console.log(`Final result: Player's won ${playerCounter} : ${computerCounter}!`);
+  else if (playerCounter === 5 && computerCounter === 5)
+    console.log('Final result: It\'s a tie!');
+  else
+    console.log(`Final result: Computer's won ${computerCounter} : ${playerCounter}!`);
+}
+
+game();
+
+
+//playOneRound Method 1: using if else
+// function playOneRound(playerChoice, computerSelection){
+//   let playerSelection = playerChoice.toLowerCase();
+//   console.log(playerSelection);
+//   console.log(computerSelection);
+//   if (playerSelection === computerSelection)
+//     return 'It\'s a tie!';
+//   else if (playerSelection === 'rock' && computerSelection === 'scissors')
+//     return `You win! ${playerSelection} beats ${computerSelection}`;
+//   else if (computerSelection === 'rock' && playerSelection === 'scissors')
+//     return `You lose! ${computerSelection} beats ${playerSelection}`;
+//   else if (playerSelection === 'paper' && computerSelection === 'rock')
+//     return `You win! ${playerSelection} beats ${computerSelection}`;
+//   else if (computerSelection === 'paper' && playerSelection === 'rock')
+//     return `You lose! ${computerSelection} beats ${playerSelection}`;
+//   else if (playerSelection === 'scissors' && computerSelection === 'paper')
+//     return `You win! ${playerSelection} beats ${computerSelection}`;
+//   else 
+//     return `You lose! ${computerSelection} beats ${playerSelection}`;
+// }
