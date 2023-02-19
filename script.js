df006ee3ebb9b24d2db9f46909d85d7eae4bdeb7
Wenwen Wang <wenwen.wangwen@gmail.com>
2023-02-19 10:12:08 -0800

    Add a message to be displayed when a round is tied

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const buttons = document.querySelectorAll('.btn-container');
+const updateMessage = document.querySelector('#updateMessage');
 
 let playerSelection;
 let computerSelection;
@@ -12,25 +13,38 @@ function game(e) {
   playerSelection = e.target.id;
   let result = playRound();
 
-  if (result === 1)
+  if (result === 2) {
+    updateMessage.textContent = `It's a tie!`
+  }
+
+  if (result === 1) {
     playerScoreboard.textContent = Number(playerScoreboard.textContent) + 1;
-  else if (result === 0)
+    updateMessage.textContent = `You won this round, good job!`
+  }
+  else if (result === 0) {
     computerScoreboard.textContent = Number(computerScoreboard.textContent) + 1;
+    updateMessage.textContent =  `Uh oh, the computer has won this round!`
+  }
 
   if (Number(playerScoreboard.textContent) === 5)  {
     document.querySelector('h2').textContent = 'You won!';
+    return;
   }
   if (Number(computerScoreboard.textContent) === 5) {
     document.querySelector('h2').textContent = 'Computer won!';
+    return;
   }
+
 }
 
 function playRound(){
   computerSelection = getComputerChoice();
-  console.log(computerSelection);
-  console.log(playerSelection);
+  console.log('computer: ', computerSelection);
+  console.log('player: ', playerSelection);
 
   switch(true) {
+    case playerSelection === computerSelection:
+      return 2;
     case playerSelection === 'rock' && computerSelection === 'scissors':
     case playerSelection === 'paper' && computerSelection === 'rock':
     case playerSelection === 'scissors' && computerSelection === 'paper':
